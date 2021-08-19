785b629f1c02dd20e53c86eca83b7b80e710d7a9
CIANDT\alexrebello <alexrebello@ciandt.com>
2021-08-19 16:19:30 -0300

    Invertendo ordem de autenticação

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,17 +30,14 @@ publishing {
     }
     repositories {
         maven {
-            url =
-                uri(if (System.getenv("ARTIFACTS_ENV_ACCESS_URI") != null) System.getenv("ARTIFACTS_ENV_ACCESS_URI") else " ")
+            url = uri(if (System.getenv("ARTIFACTS_ENV_ACCESS_URI") != null) System.getenv("ARTIFACTS_ENV_ACCESS_URI") else " ")
             name = "poc"
-            credentials {
-                username =
-                    if (System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") != null) System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") else " "
-                password =
-                    if (System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") != null) System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") else " "
-            }
             authentication {
                 create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = if (System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") != null) System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") else " "
+                password = if (System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") != null) System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") else " "
             }
         }
     }
@@ -48,14 +45,11 @@ publishing {
 
 repositories {
     maven {
-        url =
-            uri(if (System.getenv("ARTIFACTS_ENV_ACCESS_URI") != null) System.getenv("ARTIFACTS_ENV_ACCESS_URI") else " ")
+        url = uri(if (System.getenv("ARTIFACTS_ENV_ACCESS_URI") != null) System.getenv("ARTIFACTS_ENV_ACCESS_URI") else " ")
         name = "poc"
         credentials {
-            username =
-                if (System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") != null) System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") else " "
-            password =
-                if (System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") != null) System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") else " "
+            username = if (System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") != null) System.getenv("ARTIFACTS_ENV_ACCESS_USERNAME") else " "
+            password = if (System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") != null) System.getenv("ARTIFACTS_ENV_ACCESS_TOKEN") else " "
         }
         authentication {
             create<BasicAuthentication>("basic")
